f1c11a08b31d4b3e5790b0481b0015f8cc0f1050
Ben Foxall <benfoxall@gmail.com>
2022-03-26 17:37:31 +0000

    All things

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-console.log("Helo")
-
 import { MP4Demuxer } from "./mp4/MP4Demuxer.js";
 
 const path = '/motorbike.mp4'
-
 
 let demuxer = new MP4Demuxer(path);
 
@@ -26,7 +23,7 @@ setInterval(render, 100)
 
 
 
-let decoder = new VideoDecoder({
+const decoder = new VideoDecoder({
     output: async (frame) => {
 
         const bitmap = await createImageBitmap(
@@ -55,7 +52,7 @@ const chunks = await new Promise(resolve => {
     const chunks = []
     demuxer.start((chunk) => {
 
-        if (chunks.push(chunk) === 100) {
+        if (chunks.push(chunk) === 150) {
             resolve(chunks.slice(0))
         }
     })
@@ -64,14 +61,45 @@ const chunks = await new Promise(resolve => {
 
 console.log(chunks)
 
-chunks.splice(1, 20)
+function chop(array) {
 
-
-
-
-
-for (const chunk of chunks) {
-    decoder.decode(chunk);
 }
 
+
+function randomise(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function randomiseByX(input, x) {
+
+    const slices = Array.from(Array(Math.floor(input.length / x)).keys()).map(v => v * x);
+    randomise(slices);
+    console.log(slices);
+    let output = [];
+
+    for (let i = 0; i < slices.length; i++) {
+        const v = slices[i];
+
+        const slice = input.slice(v, Math.min(v + x, input.length));
+
+        output = [...output, ...slice];
+    }
+
+    return output;
+}
+
+const key = chunks.shift();
+console.log(key)
+const shuffledChunks = randomiseByX(chunks, 15);
+console.log(shuffledChunks);
+shuffledChunks.unshift(key);
+
+for (const chunk of shuffledChunks) {
+    decoder.decode(chunk);
+}
 
