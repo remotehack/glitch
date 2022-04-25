f90de2f24bbe52e6b41b59fefc3cdad20cc1c14b
Ben Foxall <benfoxall@gmail.com>
2022-04-25 17:54:26 +0000

    UGLY but working (ish)

--- src/js/frames.js
+++ src/js/frames.js
@@ -25,6 +25,7 @@ videoInput.addEventListener('change', () => run(videoInput.value))
 Sortable.create(frameList, { multiDrag: true })
 
 
+let config;
 const chunks = []
 
 async function run(path) {
@@ -32,7 +33,7 @@ async function run(path) {
     // load video 
     let demuxer = new MP4Demuxer(path);
 
-    const config = await demuxer.getConfig();
+    config = await demuxer.getConfig();
     console.log(config)
 
     frameList.innerHTML = ''
@@ -53,32 +54,108 @@ async function run(path) {
     }, true);
 
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-
-        const ordered = []
-        let timestamp = 0;//or 2048?
-
-        for (const child of frameList.children) {
-            const sample = chunks[child.dataset.idx]
-            const type = sample.is_sync ? "key" : "delta"
-
-            const chunk = new EncodedVideoChunk({
-                type: type,
-                timestamp: sample.cts,
-                duration: sample.duration,
-                data: sample.data,
-            });
-
-            ordered.push(chunk)
-
-            timestamp += chunk.duration
-        }
-
-        console.log("ORDERED", ordered)
-    })
-
 }
 
 
+// import WebMWriter from './mp4/webm-writer2.js';
 
+let currentInterval;
+
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+
+    // const maxFrames = 150;
+
+    const canvas = document.createElement("canvas");
+    canvas.width = config.codedWidth
+    canvas.height = config.codedHeight
+    document.body.append(canvas)
+    const ctx = canvas.getContext("2d");
+    const frames = [];
+    let f = 0;
+    function render() {
+        const frame = frames[f++ % frames.length];
+
+        if (frame) {
+            ctx.drawImage(frame, 0, 0);
+        }
+    }
+    currentInterval = setInterval(render, 50);
+
+    const decoder = new VideoDecoder({
+        output: async (frame) => {
+            const bitmap = await createImageBitmap(frame);
+            frames.push(bitmap);
+            frame.close();
+        },
+        error: (e) => logger.log(`Error: ${e}`)
+    });
+
+    // const config = await demuxer.getConfig();
+
+    decoder.configure(config);
+
+    // const chunks = await new Promise((resolve) => {
+    //     const chunks = [];
+    //     demuxer.start((chunk) => {
+    //         if (chunks.push(chunk) === maxFrames) {
+    //             resolve(chunks.slice(0));
+    //         }
+    //     });
+    // });
+
+
+
+    // const webmWriter = new WebMWriter({
+    //     fileWriter: null,
+    //     codec: 'VP9',
+    //     width: config.codedWidth,
+    //     height: config.codedHeight
+    // });
+
+    const ordered = []
+    let timestamp = 2048;// 0;//or 2048?
+
+    for (const child of frameList.children) {
+        const sample = chunks[child.dataset.idx]
+        const type = sample.is_sync ? "key" : "delta"
+
+        const chunk = new EncodedVideoChunk({
+            type: type,
+            timestamp: timestamp,
+            duration: sample.duration,
+            data: sample.data,
+        });
+
+        ordered.push(chunk)
+
+
+        decoder.decode(chunk)
+
+        // webmWriter.addFrame(chunk);
+
+        timestamp += chunk.duration
+        console.log(timestamp)
+    }
+
+    console.log("ORDERED", ordered)
+
+
+    // const blob = await webmWriter.complete()
+
+    // console.log(blob)
+    // // debugger;
+    // const url = URL.createObjectURL(blob)
+    // console.log(url)
+
+    // const video = document.createElement('video')
+    // video.controls = true
+    // // video.srcObject = blob;
+    // video.src = url;
+
+    // document.body.append(video)
+
+
+})
